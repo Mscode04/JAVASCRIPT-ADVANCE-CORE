46b677ca34383ad45a732359c6951caac7715d64
Mscode04 <ms.shaheenkp@gmail.com>
2024-08-12 09:48:26 +0530

    completed

---
+++ DataTypes/Exponent.js
@@ -0,0 +1,9 @@
+// Exponential Notation
+
+
+
+// Extra large or extra small numbers can be written with scientific (exponential) notation:
+
+Example
+let y = 123e5;    // 12300000
+let z = 123e-5;   // 0.00123
