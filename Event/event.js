1a292359f1ac0df12c4a7e22e5a21183c9e53347
Mscode04 <ms.shaheenkp@gmail.com>
2024-08-13 10:05:09 +0530

    ms-commit

---
+++ Event/event.js
@@ -0,0 +1,16 @@
+// HTML allows event handler attributes, with JavaScript code, to be added to HTML elements.
+// {/* <element event="some JavaScript">  */}
+
+
+// Onclick
+{/* <button onclick="displayDate()">The time is?</button> */}
+
+// Event	    Description
+// onchange	An  HTML element has been changed
+// onclick	    The user clicks an HTML element
+// onmouseover	The user moves the mouse over an HTML element
+// onmouseout	The user moves the mouse away from an HTML element
+// onkeydown	The user pushes a keyboard key
+// onload	    The browser has finished loading the page
+
+
